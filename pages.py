344039c0d9a6882d72a5e1ecafcd956117a69900
Martijn Berntsen <mxberntsen@gmail.com>
2017-03-09 22:30:17 +0100

    - improved module search - added action filtering

--- pages.py
+++ pages.py
@@ -7,7 +7,7 @@ import simplejson
 import subprocess
 import re
 import os
-import imp
+import sys
 
 class PageMaker(uweb.DebuggingPageMaker):
   """Holds all the html generators for the webapp
@@ -21,38 +21,43 @@ class PageMaker(uweb.DebuggingPageMaker):
     with open('/home/martijn/.uweb/sites.json', 'r') as f:
       sites = simplejson.load(f)
     if self.post.getfirst('action'):
-      action = str(self.post.getfirst('action'))
-      sitename = self.post.getfirst('site')
-      if sitename != 'uwebmanager':
-        router = sites[sitename]['router']
-        workdir = sites[sitename]['workdir']
-        subprocess.Popen(['python', '-m', router, action], cwd=workdir).wait()
+      action = self.post.getfirst('action')
+      if action in ('start', 'stop', 'restart'):
+        sitename = self.post.getfirst('site')
+        if sitename != 'uwebmanager':
+          router = sites[sitename]['router']
+          workdir = sites[sitename]['workdir']
+          subprocess.Popen(['python', '-m', router, action], cwd=workdir).wait()
+        else:
+          message = 'Cannot stop manager itself :)'
+      else:
+        message = 'Invalid action (%s)' % action
 
     sites2 = []
     for key, value in sites.items():
       value['key'] = key
-      #parts = value['router'].split('.')
-      #a, b, c = imp.find_module(parts[0], [value['workdir']])
-      #for part in parts[1:]:
-      #  a, b, c = imp.find_module(part, b)
-      #asdf
-      filename = value['workdir'] + '/' + value['router'].replace('.', '/') + '.py'
-      try:
-        with open(filename, 'r') as f:
-          code = f.read()
-        m = re.search('PACKAGE = \'([a-z_]*)\'', code)
-        packagename = m.group(1)
-        routername = value['router'].split('.')[-1]
-        pidfile = '/var/lock/underdark/' + packagename + '/' + routername + '.pid'
-        if os.path.exists(pidfile):
-          value['status'] = 'Running'
-          value['class'] = 'success'
-        else:
-          value['status'] = 'Stopped'
-          value['class'] = 'error'
-      except:
-        value['status'] = 'Dunno'
-        value['class'] = ''
+      paths = [value['workdir']] + sys.path
+      for path in paths:
+        filename = path + '/' + value['router'].replace('.', '/') + '.py'
+        try:
+          with open(filename, 'r') as f:
+            code = f.read()
+          m = re.search('PACKAGE = \'([a-z_]*)\'', code)
+          packagename = m.group(1)
+          routername = value['router'].split('.')[-1]
+          pidfile = '/var/lock/underdark/' + packagename + '/' + routername + '.pid'
+          if os.path.exists(pidfile):
+            value['status'] = 'Running'
+            value['class'] = 'success'
+          else:
+            value['status'] = 'Stopped'
+            value['class'] = 'error'
+          break
+        except IOError:
+          pass
+        except:
+          value['status'] = 'Dunno'
+          value['class'] = ''
       sites2 = sites2 + [value]
     return self.parser.Parse('index.utp', sites=sites2, message=message)
 
